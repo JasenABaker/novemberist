6bca2d971b7eab645a299beb02f52358e1542846
JasenABaker <JasenABaker@gmail.com>
2018-06-20 16:08:44 -0400

    set up controllers for models

--- app/controllers/api/shorts_controller.rb
+++ app/controllers/api/shorts_controller.rb
@@ -1,22 +1,26 @@
 class Api::ShortsController < ApplicationController
     def index
-        @shorts = Short.all
+        @collection = Collection.find(params[:collection_id])
+        @shorts = @collection.shorts
         render json: @shorts
     end
 
     def create
-        @short = Short.create(short_params)
+        @collection = Collection.find(params[:collection_id])
+        @short = @collection.shorts.create(short_params)
 
         render json: @short
     end
 
     def show
-        @short = Short.find(params[:id])
+        @collection = Collection.find(params[:collection_id])
+        @short = @collection.shorts.id(params[:id])
         render json: @short
     end
 
     def update
-        @short = Short.find(params[:id])
+        @collection = Collection.find(params[:collection_id])
+        @short = @collection.shorts.id(params[:id])
         @short.update!(short_params)
         render json: @short
     end
--- app/controllers/api/wips_controller.rb
+++ app/controllers/api/wips_controller.rb
@@ -1,2 +1,35 @@
 class Api::WipsController < ApplicationController
+    def index
+        @wips = Wip.all
+        render json: @wips
+    end
+
+    def create
+        @wip = Wip.create(wip_params)
+
+        render json: @wip
+    end
+
+    def show
+        @wip = Wip.find(params[:id])
+        render json: @wip
+    end
+
+    def update
+        @wip = Wip.find(params[:id])
+        @wip.update!(wip_params)
+        render json: @wip
+    end
+
+    def destroy
+        @wip = Wip.find(params[:id]).delete
+
+        render status: :ok
+    end
+
+    private
+
+    def wip_params
+        params.require(:wip).permit(:title, :content)
+    end
 end
