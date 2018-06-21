06f877dcf547f617387638d07ca9d46a5703d5b7
JasenABaker <JasenABaker@gmail.com>
2018-06-21 18:07:40 -0400

    Seeded database

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependant: :destroy
+  has_many :comments, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Blog.destroy_all
+Collection.destroy_all
+Wip.destroy_all
+
+
+admin = User.create!({
+    email: "JasenABaker@gmail.com",
+    nickname: "Admin",
+    password: "1873jbbb",
+    encrypted_password: "1873jbbb",
+    name: "Jasen Baker",
+    admin: true,
+    image: "https://i.imgur.com/ZNTTFCp.jpg",
+})
+
+first = admin.blogs.create!({
+    title: "First Blog",
+    content: "Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong.
+
+    Tail short loin filet mignon tenderloin cow, pastrami meatball kevin. Chicken ham tri-tip, rump pork belly capicola filet mignon frankfurter sausage ball tip bresaola. Porchetta fatback hamburger, bresaola cupim buffalo drumstick burgdoggen beef. Jerky meatball pig bresaola, shoulder alcatra leberkas pork flank tongue.
+    
+    Capicola tongue chuck flank andouille beef ribs hamburger picanha meatloaf fatback salami short ribs turkey. Kevin cupim swine bacon fatback pork belly shankle, pancetta porchetta ribeye. Boudin leberkas filet mignon pancetta capicola t-bone sausage hamburger sirloin. Spare ribs shank drumstick boudin jowl pork. Kielbasa pork loin jowl pork chop shank, ribeye bresaola.
+    
+    Strip steak sirloin bresaola meatloaf cow filet mignon boudin turducken. Spare ribs short loin frankfurter bresaola pig venison salami ham hock burgdoggen. Corned beef beef ribs fatback burgdoggen boudin shank t-bone strip steak short loin ball tip frankfurter kielbasa swine meatloaf bresaola. Shankle kevin chuck short ribs swine, pig spare ribs cow flank filet mignon pancetta jowl meatball. Tongue tail capicola burgdoggen beef ribs andouille biltong ground round turkey picanha. Capicola rump alcatra filet mignon. Meatloaf strip steak ground round sirloin."
+})
+
+Tales = admin.collections.create!({
+    title: "Tales From The Fence",
+    info: "A collection of stories inspired by the ablum, Good Apollo volume: I, by the band Coheed and Cambria."
+
+})
+always = Tales.shorts.create!({
+    title:"Always and Never",
+    content: "Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong.
+
+    Tail short loin filet mignon tenderloin cow, pastrami meatball kevin. Chicken ham tri-tip, rump pork belly capicola filet mignon frankfurter sausage ball tip bresaola. Porchetta fatback hamburger, bresaola cupim buffalo drumstick burgdoggen beef. Jerky meatball pig bresaola, shoulder alcatra leberkas pork flank tongue.
+    
+    Capicola tongue chuck flank andouille beef ribs hamburger picanha meatloaf fatback salami short ribs turkey. Kevin cupim swine bacon fatback pork belly shankle, pancetta porchetta ribeye. Boudin leberkas filet mignon pancetta capicola t-bone sausage hamburger sirloin. Spare ribs shank drumstick boudin jowl pork. Kielbasa pork loin jowl pork chop shank, ribeye bresaola.
+    
+    Strip steak sirloin bresaola meatloaf cow filet mignon boudin turducken. Spare ribs short loin frankfurter bresaola pig venison salami ham hock burgdoggen. Corned beef beef ribs fatback burgdoggen boudin shank t-bone strip steak short loin ball tip frankfurter kielbasa swine meatloaf bresaola. Shankle kevin chuck short ribs swine, pig spare ribs cow flank filet mignon pancetta jowl meatball. Tongue tail capicola burgdoggen beef ribs andouille biltong ground round turkey picanha. Capicola rump alcatra filet mignon. Meatloaf strip steak ground round sirloin."
+})
+
+pirate = admin.wips.create!({
+    title: "Pirates of Calypso",
+    content:"Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong.
+
+    Tail short loin filet mignon tenderloin cow, pastrami meatball kevin. Chicken ham tri-tip, rump pork belly capicola filet mignon frankfurter sausage ball tip bresaola. Porchetta fatback hamburger, bresaola cupim buffalo drumstick burgdoggen beef. Jerky meatball pig bresaola, shoulder alcatra leberkas pork flank tongue.
+    
+    Capicola tongue chuck flank andouille beef ribs hamburger picanha meatloaf fatback salami short ribs turkey. Kevin cupim swine bacon fatback pork belly shankle, pancetta porchetta ribeye. Boudin leberkas filet mignon pancetta capicola t-bone sausage hamburger sirloin. Spare ribs shank drumstick boudin jowl pork. Kielbasa pork loin jowl pork chop shank, ribeye bresaola.
+    
+    Strip steak sirloin bresaola meatloaf cow filet mignon boudin turducken. Spare ribs short loin frankfurter bresaola pig venison salami ham hock burgdoggen. Corned beef beef ribs fatback burgdoggen boudin shank t-bone strip steak short loin ball tip frankfurter kielbasa swine meatloaf bresaola. Shankle kevin chuck short ribs swine, pig spare ribs cow flank filet mignon pancetta jowl meatball. Tongue tail capicola burgdoggen beef ribs andouille biltong ground round turkey picanha. Capicola rump alcatra filet mignon. Meatloaf strip steak ground round sirloin.",
+    draft: "Rough",
+    percent: 90
+
+})
+
+Comment.create(
+    wip: pirate,
+    user: admin,
+    content:"Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong."
+)
+Comment.create(
+    short: always,
+    user: admin,
+    content:"Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong."
+)
+Comment.create(
+    blog: first,
+    user: admin,
+    content:"Bacon ipsum dolor amet jowl cupim tenderloin, frankfurter filet mignon meatball flank ribeye shankle. Bacon jowl short ribs sausage, pork belly tail andouille hamburger capicola rump pig shoulder. Picanha drumstick cupim meatloaf flank brisket tenderloin tri-tip capicola short loin leberkas swine tongue bacon burgdoggen. Swine chicken shank, fatback beef alcatra short ribs rump biltong."
+)
