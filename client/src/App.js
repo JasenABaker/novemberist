9a6a7396fe2a1519845545de0dce0e42921509cc
JasenABaker <JasenABaker@gmail.com>
2018-06-21 22:54:11 -0400

    Created and styled header

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import {Header, TitleDiv, NavBar} from './components/styled_components/header'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header>
+          <TitleDiv>
+            <h2>The</h2>
+            <h1>Novemberist</h1>
+          </TitleDiv>
+          <NavBar>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Shorts</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">SignIn</a></li>
+            </NavBar>
+        </Header>
+        Hello from App
       </div>
     );
   }
---
+++ client/src/components/styled_components/header.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const Header = styled.div`
+    height: 10vh;
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+    color: #EAF4F4;
+    background-color: #011627;
+    font-family: 'Oleo Script', cursive;
+`
+
+export const TitleDiv = styled.div`
+    height: 100%;
+    width: 30vw;
+    display: flex;
+
+    h1{
+        align-self: flex-end;
+        font-size: 2.8em;
+        margin-bottom:0;
+    }
+    h2{
+        align-self: flex-start;
+    }
+`
+
+export const NavBar = styled.div`
+    height: 100%;
+    width: 45vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    list-style-type: none;
+
+    a{
+        color: #EAF4F4;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-family: 'Oswald', sans-serif;
+        letter-spacing: 1px;
+
+        :hover{
+            color: #F2545B;
+        }
+    }
+
+`
