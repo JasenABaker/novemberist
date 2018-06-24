caaec2f7730cd839a24761d5adc34a55950b747a
JasenABaker <JasenABaker@gmail.com>
2018-06-23 21:51:32 -0400

    created carousel for shorts

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { MainPage, CarouselContainer, SecHomeCon, BlogCon } from './styled_components/container'
-import {Carousel} from 'react-responsive-carousel'
+import { MainPage, CarouselContainer, SecHomeCon, BlogCon, Children, Dot, Dots, Arrow } from './styled_components/container'
+import makeCarousel from 'react-reveal/makeCarousel'
+import Slide from 'react-reveal/Slide'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import Image from './styled_components/images/nasa.jpg'
+
+
+const CarouselUI = ({ position, total, handleClick, children }) => (
+    <CarouselContainer>
+        <Children>
+            {children}
+            <Arrow onClick={handleClick} data-position={position - 1}>{'<'}</Arrow>
+            <Arrow right onClick={handleClick} data-position={position + 1}>{'>'}</Arrow>
+        </Children>
+        <Dots>
+            {Array(...Array(total)).map((val, index) =>
+                <Dot key={index} onClick={handleClick} data-position={index}>
+                    {index === position ? '● ' : '○ '}
+                </Dot>
+            )}
+        </Dots>
+    </CarouselContainer>
+)
+const Carousel = makeCarousel(CarouselUI)
 
 class Home extends Component {
     state = {
         blogs: [],
         shorts: [],
+        collections: [],
         isloaded: false,
     }
     async componentWillMount() {
@@ -22,8 +43,9 @@ class Home extends Component {
             const shortSort = resShorts.data.sort((a, b) => {
                 return new Date(b.created_at) - new Date(a.created_at)
             })
+            const resCollect = await axios.get('/api/collections')
             const shortSplice = shortSort.splice(0, 4)
-            this.setState({ blogs: blogSplice, shorts: shortSplice, isloaded: true })
+            this.setState({ blogs: blogSplice, shorts: shortSplice, collections: resCollect.data, isloaded: true })
         }
         catch (err) {
             console.log(err)
@@ -35,38 +57,37 @@ class Home extends Component {
         const shorts = this.state.shorts
         return (
             this.state.isloaded ?
-            <MainPage>
-                <CarouselContainer>
-                <Carousel showArrows={true} showThumbs={false} infiniteLoop={true} transitionTime={1000} interval={5000} autoPlay={true}
-                showStatus={false} showIndicators={false} dynamicHeight={true}>
-                {this.state.shorts.map((short)=>{
-                    return(
-                    <div>
-                        <img src={Image} />
-                        <h1>{short.title}</h1>
-                        <p className="legend">{short.content}</p>
-                        <button> Read More</button>
-                    </div>
-                    )
-                    })}
+                <MainPage>
+                    <Carousel defaultWait={7000}>
+                        {this.state.shorts.map((short) => {
+                            return (
+                                <Slide right>
+                                    <div>
+                                        <h1>{short.title}</h1>
+                                        <p>{short.content}</p>
+                                        <button>Read More</button>
+                                    </div>
+                                </Slide>
+                            )
+                        })}
                     </Carousel>
-                    </CarouselContainer>
+
                     <SecHomeCon>
-                    {this.state.blogs.map((blog)=>{
-                        let date = new Date(blog.created_at)
-                        return(
-                        <BlogCon>
-                            <h1>{blog.title}</h1>
-                            <h4>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</h4>
-                            <p>{blog.content}</p>
-                            <button>Read More</button>
-                            
-                        </BlogCon>
-                        )
-                    })}
+                        {this.state.blogs.map((blog) => {
+                            let date = new Date(blog.created_at)
+                            return (
+                                <BlogCon>
+                                    <h1>{blog.title}</h1>
+                                    <h4>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</h4>
+                                    <p>{blog.content}</p>
+                                    <button>Read More</button>
+
+                                </BlogCon>
+                            )
+                        })}
                     </SecHomeCon>
-            </MainPage>
-            : <img src="https://media.giphy.com/media/xT9DPldJHzZKtOnEn6/giphy.gif" alt="Loading"/>
+                </MainPage>
+                : null
         )
     }
 }
--- client/src/components/styled_components/container.jsx
+++ client/src/components/styled_components/container.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components';
 
 
 
@@ -41,8 +41,6 @@ export const InsideAside = styled.div`
         font-size: 16px;
     }
 `
-
-
 export const AsideImg = styled.div`
     height: 200px;
     width: 200px;
@@ -58,15 +56,61 @@ export const AsideImg = styled.div`
 `
 
 export const CarouselContainer = styled.div`
+    height: 30vh;
     width: 60vw;
     margin-top: 30px;
+    position: relative;
+    overflow: hidden;
+
+    h1{
+        padding-left: 20px;
+        margin: 0;
+    }
+    p{
+        height: 10vh;
+        width: 55vw;
+        padding-left: 20px;
+        padding-bottom: 5px;
+        overflow: hidden;
+        margin: 10 0 0 0;
+    }
+
+`
+const width = '60vw', height='25vh';
+export const Children  = styled.div`
+  width: ${width};
+  position: relative;
+  height: ${height};
+`
+export const Arrow = styled.div`
+  text-shadow: 1px 1px 1px #fff;
+  z-index: 100;
+  line-height: ${height};
+  text-align: center;
+  position: absolute;
+  top: 0;
+  width: 10%;
+  font-size: 2em;
+  cursor: pointer;
+  user-select: none;
+  ${props => props.right ? css`left: 90%;` : css`left: -4%;`}
+`
+export const Dot = styled.span`
+  font-size: 1.5em;
+  cursor: pointer;
+  text-shadow: 1px 1px 1px #fff;
+  user-select: none;
+`
+export const Dots = styled.span`
+  text-align: center;
+  width: ${width};
+  z-index: 100;
 `
 export const SecHomeCon = styled.div`
     margin-top: 30px;
     height: 30vh;
     width: 60vw;
 `
-
 export const BlogCon = styled.div`
     height: 100%;
     border-top: 1px solid grey;
